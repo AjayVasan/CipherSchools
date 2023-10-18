f8e9f32065451ebbe777bd58df8c2bb76854bd80
AjayVasan <92700675+AjayVasan@users.noreply.github.com>
2023-10-18 17:46:49 +0530

    Update lecture8.2.cpp

--- lecture8.2.cpp
+++ lecture8.2.cpp
@@ -5,8 +5,16 @@ int helo(){
   cout<<"hello\n";
   return 20;
 };
+int add(int a,int b){
+  cout<<"addition of "<<a<<" and "<<b<<" is: ";
+  int c=a+b;
+  return c;
+};
 
 int main(){
   int h = helo();
   cout<<"H: "<<h<<"\n";
+  int j=add(10,20);
+  cout<<j<<"\n";
 };
+
